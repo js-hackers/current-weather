9989ed9e0cd3910f79b1d1e92231822899ba72ba
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-22 15:28:56 -0600

    Setup create-react-app and basic configuration

---
+++ docs/precache-manifest.c81c830d27c56ade0d8b62a798ad9bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37183491a7a6cfdb6b7a80e3515eb9aa",
+    "url": "/current-weather/index.html"
+  },
+  {
+    "revision": "cb7f09690a67755a1e54",
+    "url": "/current-weather/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ccc64f44ec657e958e1a",
+    "url": "/current-weather/static/js/2.facf0f99.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/current-weather/static/js/2.facf0f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7f09690a67755a1e54",
+    "url": "/current-weather/static/js/main.a288535d.chunk.js"
+  },
+  {
+    "revision": "4dc789d706593cc5fbc2",
+    "url": "/current-weather/static/js/runtime-main.e1bf8379.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/current-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
