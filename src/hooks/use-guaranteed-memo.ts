c95cf698f7ae37a578861f95bb625d3fd2b7320e
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-28 11:09:46 -0600

    Add hooks: useGuaranteedMemo, usePrevious
    useGuaranteedMemo will be the basis for the useStyles hook

---
+++ src/hooks/use-guaranteed-memo.ts
@@ -0,0 +1,31 @@
+import {useEffect, useRef} from 'react';
+
+const usePrevious = <T>(value: T): T => {
+  const ref = useRef(value);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+type Dependencies = ReadonlyArray<any>; // eslint-disable-line @typescript-eslint/no-explicit-any, max-len
+
+export const useGuaranteedMemo = <T>(
+  factory: (...dependencies: Dependencies) => T,
+  dependencies: Dependencies,
+): ReturnType<typeof factory> => {
+  const result = useRef<T>(factory(...dependencies));
+  const dPrevious = usePrevious(dependencies);
+
+  let areEqual = dependencies.length === dPrevious.length;
+
+  for (let i = 0; i < dependencies.length && areEqual; i += 1) {
+    areEqual = Object.is(dependencies[i], dPrevious[i]);
+  }
+
+  if (!areEqual) result.current = factory(...dependencies);
+
+  return result.current;
+};
+
+export default useGuaranteedMemo;
---
+++ src/hooks/use-previous.ts
@@ -0,0 +1,11 @@
+import {useEffect, useRef} from 'react';
+
+export const usePrevious = <T>(value: T): T => {
+  const ref = useRef(value);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export default usePrevious;
