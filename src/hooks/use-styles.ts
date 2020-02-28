7c9d0088fd54b9611bcda626a2cc03ae0ae34259
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-28 11:31:24 -0600

    Recycle useGuaranteedMemo hook; include type alias

--- src/hooks/use-styles.ts
+++ src/hooks/use-styles.ts
@@ -1,31 +1,8 @@
 import {css, keyframes} from '@emotion/core';
-import {useMemo} from 'react';
+export {useGuaranteedMemo as useMemoizedStyles} from './use-guaranteed-memo';
 
-// export type Styles = {[key: string]: SerializedStyles};
 export type Styles = {
   [key: string]:
     | ReturnType<typeof css>
     | ReturnType<typeof keyframes>;
 };
-
-type Dependencies = any[]; // eslint-disable-line @typescript-eslint/no-explicit-any, max-len
-export type CreateStyles = (...dependencies: Dependencies) => Styles;
-
-export const useStyles =
-  (createStyles: CreateStyles, dependencies?: Dependencies): Styles => useMemo(
-    () => createStyles(...(
-      dependencies !== undefined
-      && dependencies.length > 0
-    )
-      ? dependencies
-      : []),
-    [
-      createStyles,
-      ...(
-        dependencies !== undefined
-        && dependencies.length > 0
-      )
-        ? dependencies
-        : [],
-    ],
-  );
