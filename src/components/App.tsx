c7213ed113e10bd506afb3a89dd03bece78b203f
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-03-03 13:01:48 -0600

    Add example using state context hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 /** @jsx jsx */
 import {E} from '../types';
 import {jsx} from '@emotion/core';
+import {setColor} from '../store';
+import {useEffect} from 'react';
+import {useStateContext} from '../hooks';
 
-export const App = (): E => <div></div>;
+export const App = (): E => {
+  const [state, dispatch] = useStateContext();
+  useEffect(() => {
+    dispatch(setColor('#ff0000'));
+  }, [dispatch]);
+  return <div>{state.color}</div>;
+};
 
 export default App;
